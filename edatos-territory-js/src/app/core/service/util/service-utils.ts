9ecd0e3cc5774fec7accaa90a82f74f6ebcf20dc
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2022-04-26 14:18:30 +0100

    EDATOS-3572 - Creación de la plantilla para el proyecto
    > Documenta función `instantiate`.

--- edatos-territory-js/src/app/core/service/util/service-utils.ts
+++ edatos-territory-js/src/app/core/service/util/service-utils.ts
@@ -2,8 +2,8 @@ import { plainToInstance } from "class-transformer";
 import { OperatorFunction, map } from "rxjs";
 
 /**
- * TODO: Document
- * @param type
+ * Converts a plain object to an instance of the given class.
+ * @param type Class of the object to be instantiated.
  */
 export function instantiate<T>(type: { new (): T }): OperatorFunction<object, T> {
     return map((res) => plainToInstance(type, res));
