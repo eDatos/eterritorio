b7ca4ab1d0e20a35749e18acfb033dedd116789e
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2022-07-06 11:12:12 +0100

    EDATOS-3675 - Añadir navbar con nombre de la app
    > Añade navbar blanca con título.

--- edatos-territory-js/src/app/modules/layout/header/header.component.ts
+++ edatos-territory-js/src/app/modules/layout/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit } from "@angular/core";
 
+import { TranslateService } from "@ngx-translate/core";
+
 import { HtmlService } from "@app/core/service";
 import { reinsertScripts } from "@app/modules/layout";
 
@@ -10,10 +12,15 @@ import { reinsertScripts } from "@app/modules/layout";
 export class HeaderComponent implements OnInit {
     headerHtml = "";
 
-    constructor(private elementRef: ElementRef, private htmlService: HtmlService) {}
+    constructor(
+        private elementRef: ElementRef,
+        private htmlService: HtmlService,
+        private translateService: TranslateService
+    ) {}
 
     ngOnInit() {
-        this.htmlService.getHeaderHtml().subscribe((data) => {
+        const appName = this.translateService.instant("app.name");
+        this.htmlService.getHeaderHtml(appName).subscribe((data) => {
             this.headerHtml = data.toString();
             setTimeout(() => {
                 reinsertScripts(this.elementRef);
