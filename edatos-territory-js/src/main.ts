48b916f28cd26177ecf6c8731f76487d46a9f01e
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2022-07-08 10:02:50 +0100

    EDATOS-3650 - Implementar un spinner al inicio de la aplicación mientras carga las propiedades
    > Añade un spinner a la app mientras carga las propiedades y un mensaje de error
    en caso de que se produzca un error.

--- edatos-territory-js/src/main.ts
+++ edatos-territory-js/src/main.ts
@@ -1,9 +1,11 @@
-import "reflect-metadata"; // needed for class-transformer
-
+// needed for class-transformer
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
+import "reflect-metadata";
+
 import { AppModule } from "@app/app.module";
+
 import { environment } from "./environments/environment";
 
 if (environment.production) {
@@ -12,4 +14,23 @@ if (environment.production) {
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
+    .catch((err) => {
+        console.error(err);
+        showErrorMessage(err);
+    });
+
+function showErrorMessage(err: any) {
+    const loaderDiv = document.getElementById("loader");
+    if (loaderDiv) {
+        loaderDiv.style.display = "none";
+    }
+
+    const errorDiv = document.getElementById("error");
+    if (errorDiv) {
+        errorDiv.style.display = "block";
+        const stacktraceDiv = document.getElementById("stacktrace");
+        if (stacktraceDiv) {
+            stacktraceDiv.textContent = err.message;
+        }
+    }
+}
