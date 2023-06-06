677d28b1c6c4671f5b296f0309bb83f69da33eb8
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2023-06-06 13:07:09 +0100

    EDATOS-4116 - La internacionación de eTerritory no funciona correctamente
    > Añade nueva propiedad del nombre de la cookie del idioma.
    > Actualiza el idioma de la app según la cookie y atiende a los cambios del selector de idiomas del header.
    > Añade dependencia para gestionar las cookies.

--- edatos-territory-js/src/app/modules/layout/header/header.component.ts
+++ edatos-territory-js/src/app/modules/layout/header/header.component.ts
@@ -3,9 +3,11 @@ import { Component, ElementRef, Inject, OnInit, Renderer2, ViewContainerRef } fr
 
 import { TranslateService } from "@ngx-translate/core";
 
-import { HtmlService } from "@app/core/service";
+import { HtmlService, PropertiesService } from "@app/core/service";
 import { reinsertScripts } from "@app/modules/layout";
 import { TerritoryAutocompleteComponent } from "@app/shared/components/territory-autocomplete";
+import {CookieService} from "ngx-cookie-service";
+import {AVAILABLE_LANGS} from "@app/app.constants";
 
 @Component({
     selector: "app-header",
@@ -21,6 +23,9 @@ export class HeaderComponent implements OnInit {
         private translateService: TranslateService,
         private renderer: Renderer2,
         private viewContainerRef: ViewContainerRef,
+        private propertiesService: PropertiesService,
+        private cookieService: CookieService,
+        private languageService: TranslateService,
         @Inject(DOCUMENT) private document: Document
     ) {}
 
@@ -31,6 +36,7 @@ export class HeaderComponent implements OnInit {
             setTimeout(() => {
                 reinsertScripts(this.elementRef);
                 this.initializeNavbarComponents();
+                this.updateLang();
             });
         });
     }
@@ -41,5 +47,15 @@ export class HeaderComponent implements OnInit {
             this.document.getElementById(HeaderComponent.TITLE_NAVBAR_ID),
             componentRef.location.nativeElement
         );
+    }
+
+    private updateLang() {
+        const cookieName = this.propertiesService.getLanguageCookieName();
+        const cookieValue = this.cookieService.get(cookieName);
+        if (cookieValue && AVAILABLE_LANGS.includes(cookieValue)) {
+            this.languageService.use(cookieValue).subscribe(() => {
+                console.debug("Language changed to", cookieValue);
+            });
+        }
     }
 }
